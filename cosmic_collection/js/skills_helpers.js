bae7613dd02603fce5b349ba29d537ad09bc7c7e
Kuzzi <sebastiansiatkowski@gmail.com>
2025-07-03 17:00:35 -0700

    Update skills_helpers.js

--- cosmic_collection/js/skills_helpers.js
+++ cosmic_collection/js/skills_helpers.js
@@ -1313,7 +1313,7 @@ window.skills = [
       id: 12201,
       name: "Space Bending Interceptor",
       description: "Unlocks a device that intercepts space bending (both passively and when you flip over cards), which can be activated to automate flipping over cards for its duration.",
-      cost: { realmId: 5, currencyId: "egg", amount: 8e6 },
+      cost: { realmId: 5, currencyId: "egg", amount: 6e6 },
       purchased: false
     },
     {
@@ -1327,7 +1327,7 @@ window.skills = [
       id: 12203,
       name: "Interceptor Poker Automation",
       description: "Interceptor also auto-pokes black hole in addition to flipping cards.",
-      cost: { realmId: 10, currencyId: "spirit", amount: 3e8 },
+      cost: { realmId: 9, currencyId: "spirit", amount: 3e7 },
       purchased: false
     },
     {
